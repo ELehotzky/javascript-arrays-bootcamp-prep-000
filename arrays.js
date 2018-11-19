c625be5eecbd3170f87446a74bc934a294eb3e78
Erica Lehotzky <lehotzky.erica@gmail.com>
2018-11-19 03:20:04 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  array = [];
+  array[0] = element;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
